a8d2487149887644a7df8982d1af6af51db5c028
vivekgorantla <23eg105j66@anurag.edu.in>
2025-07-26 11:51:14 +0530

    added summarizer background job

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,25 @@
 "use client"
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
 
 export default function Home() { 
+  const [value,setValue] = useState("")
   const trpc = useTRPC()
-  const invoke = useMutation(trpc.invoke.mutationOptions({}))
+  const invoke = useMutation(trpc.summarize.mutationOptions({
+    onSuccess : ()=>{
+      console.log("background task completed success!!")
+    }
+  }))
+
   return (
     <>
       <div className="p-4 max-w-7xl mx-auto">
-        <Button onClick={()=>invoke.mutate({text:"vivek"})}>Invoke Background Job</Button>
+        <Input value={value} onChange={(e)=>setValue(e.target.value)}></Input>
+        <Button disabled={invoke.isPending} onClick={()=>invoke.mutate({value})}>Invoke Background Job</Button>
       </div>
     </>
-  );
+  )
 }
