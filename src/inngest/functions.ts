86b2af05d11633ca9f60f8cd0f3511c0d1a50d74
vivekgorantla <23eg105j66@anurag.edu.in>
2025-07-27 05:27:24 +0530

    setup for e2b sandbox template for a nextjs application

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,26 +1,45 @@
 import { inngest } from "./client";
 import { gemini, createAgent } from "@inngest/agent-kit";
+import { Sandbox } from '@e2b/code-interpreter'
+import { getSandbox } from "./utils";
 
-export const helloWorld = inngest.createFunction(
-  { id: "text-summarizer" },
-  { event: "summarize/text-summarizer.world" },
-  async ({ event }) => {
-    const summarizerAgent = createAgent({
+
+export const codeAgent = inngest.createFunction(
+  { id: "code-generator" },
+  { event: "code/code.generator" },
+  async ({ event,step }) => {
+    const sandboxid = await step.run("get-sandbox-id",async()=>{
+      const sandbox = await Sandbox.create("vibe-coder-vivekg2005")
+      return sandbox.sandboxId
+    })
+    console.log("Incoming event data:", event.data);
+    const prompt = event.data?.text;
+    if (!prompt) {
+      console.log("Missing prompt value in event data.");
+      return {
+        error: "No prompt provided",
+        output: `Cannot generate snippet: No input provided.`,
+      };
+    }
+    const codingAgent = createAgent({
       model: gemini({ model: "gemini-1.5-flash" }),
-      name: "Text Summarizer",
-      system: "You are a helpful AI assistant that summarizes input text clearly and concisely.",
+      name: "code-agent",
+      system: "You are an expert next.js developer. You write readable, maintable code. You write simple Next.js and React snippets",
     });
 
-    const inputText = event.data?.text ?? "No input provided.";
-
-    const { output } = await summarizerAgent.run(
-      `Summarize the following text:\n\n${inputText}`
+    const { output } = await codingAgent.run(
+      `Write the following snippet: ${prompt}`
     );
 
-    console.log("Summary:", output);
+    console.log("code:", output);
 
-    return {
-      summary: output,
-    };
+    const sandboxurl = await step.run("get-sandbox-url",async()=>{
+      const sandbox = await getSandbox(sandboxid);
+      const host =  sandbox.getHost(3000);
+      return `https://${host}`
+    })
+
+
+    return {output, sandboxurl};
   }
 );
