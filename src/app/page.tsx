347e9cae72e4f1bea508045a9e195464244f47c4
vivekgorantla <23eg105j66@anurag.edu.in>
2025-07-26 10:25:13 +0530

    inngest-setup for background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,16 @@
-import { getQueryClient,trpc } from "@/trpc/server";
-import { dehydrate,HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
-import Client from "./Client";
+"use client"
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
 
 export default function Home() { 
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.hello.queryOptions({ text: "vivek" }));
-
+  const trpc = useTRPC()
+  const invoke = useMutation(trpc.invoke.mutationOptions({}))
   return (
     <>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<p>...Loading!!</p>}>
-          <Client/>
-        </Suspense>
-      </HydrationBoundary>
+      <div className="p-4 max-w-7xl mx-auto">
+        <Button onClick={()=>invoke.mutate({text:"vivek"})}>Invoke Background Job</Button>
+      </div>
     </>
   );
 }
